ac2fcb73ecc458632c8ed413bebce11b5a979cdd
Amolnar4D41 <amolnar4d41@gmail.com>
2024-08-17 07:27:17 +0200

    Init

---
+++ src/main/java/com/storage/home/homestoreapi/JavaReactiveExampleApiApplication.java
@@ -0,0 +1,13 @@
+package com.storage.home.homestoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReactiveExampleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaReactiveExampleApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/storage/home/homestoreapi/JavaReactiveExampleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.storage.home.homestoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaReactiveExampleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
