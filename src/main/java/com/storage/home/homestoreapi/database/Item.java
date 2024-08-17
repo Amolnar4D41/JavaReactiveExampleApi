2cf1e0883209adb40ddce69dfc89bbb307343f42
Amolnar4D41 <amolnar4d41@gmail.com>
2024-08-17 08:51:07 +0200

    Lombok instead of generated pojo

--- src/main/java/com/storage/home/homestoreapi/database/Item.java
+++ src/main/java/com/storage/home/homestoreapi/database/Item.java
@@ -3,33 +3,16 @@ package com.storage.home.homestoreapi.database;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 @Entity
 @Table(name = "items")
+@Data
+@NoArgsConstructor
 public class Item {
     @Id
+    @NonNull
     private String name;
-
-    public Item() {
-
-    }
-
-    public Item(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public String toString() {
-        return "Item{" +
-                "name='" + name + '\'' +
-                '}';
-    }
 }
